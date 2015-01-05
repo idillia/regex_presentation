d655c99e5aef4f41051ec51d7568f3f7cfa95082
joshWyatt <joshuawyatt123@gmail.com>
2015-01-05 11:10:08 -0800

    Add presentation.js file

---
+++ presentation.js
@@ -0,0 +1,24 @@
+//difference between expression literal and constructor function
+
+// SPECIAL CHARACTERS
+// \
+// .
+// \w
+// \W
+// \b
+// \B
+// \d
+// \D
+// \s
+// \S
+// ^
+// $
+// * {0,}
+// + {1,}
+// ? {0,1}
+// [xyz] [a-b] [a.d] <--- not special
+// x|y
+// (x)
+// (?:x) non-capturing parenthesis
+// x(?=y) positive lookahead matches x only if followed by y
+// x(?!y) negative lookahead
